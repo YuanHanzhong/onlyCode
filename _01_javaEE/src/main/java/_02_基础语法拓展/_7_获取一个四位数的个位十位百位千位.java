7e3b26059185c711d2256c0eca7f7a0d6d95085a
YuanHanzhong <yuanhanzhong666@gmail.com>
2023-10-24 18:44:31 +0800

    Committing unstaged changes before filter-branch

--- _01_javaEE/src/main/java/_02_基础语法拓展/_7_获取一个四位数的个位十位百位千位.java
+++ _01_javaEE/src/main/java/_02_基础语法拓展/_7_获取一个四位数的个位十位百位千位.java
@@ -9,7 +9,6 @@ package _02_基础语法拓展;/*
 public class _7_获取一个四位数的个位十位百位千位 {
     public static void main(String[] args) {
         int a = 4328;
-        
         int qian = a/1000;
         int bai = a%1000/100;
         int shi = a%100/10;
